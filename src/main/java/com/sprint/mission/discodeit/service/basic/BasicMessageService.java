7960fe7f5424300ade93ebeb444716d11a3106ba
김주언 <wndjs803@naver.com>
2025-01-20 11:33:14 +0900

    feat: BasicMessageService 작성

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -0,0 +1,74 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.common.ErrorMessage;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class BasicMessageService implements MessageService {
+    private final MessageRepository messageRepository;
+    private final UserService userService;
+    private final ChannelService channelService;
+
+    public BasicMessageService(MessageRepository messageRepository, UserService userService, ChannelService channelService) {
+        this.messageRepository = messageRepository;
+        this.userService = userService;
+        this.channelService = channelService;
+    }
+
+    @Override
+    public Message createMessage(UUID sendUserId, UUID channelId, String content) {
+        User sendUser = userService.findUserByIdOrThrow(sendUserId);
+        Channel foundChannel = channelService.findChannelByIdOrThrow(channelId);
+
+        Message message = Message.of(sendUser, foundChannel, content);
+
+        return messageRepository.saveMessage(message);
+    }
+
+    @Override
+    public Message findMessageByIdOrThrow(UUID messageId) {
+        return Optional.ofNullable(messageRepository.findMessageById(messageId))
+                .orElseThrow(() -> new RuntimeException(ErrorMessage.MESSAGE_NOT_FOUND.getMessage()));
+    }
+
+    @Override
+    public List<Message> findAllMessage() {
+        return messageRepository.findAllMessages();
+    }
+
+    @Override
+    public Message updateMessage(UUID sendUserId, UUID messageId, String content) {
+        userService.findUserByIdOrThrow(sendUserId);
+        Message foundMessage = findMessageByIdOrThrow(messageId);
+
+        if (foundMessage.isNotOwner(sendUserId)) {
+            throw new RuntimeException(ErrorMessage.NOT_MESSAGE_CREATOR.getMessage());
+        }
+
+        foundMessage.updateContent(content);
+        foundMessage.updateUpdatedAt(Instant.now().toEpochMilli());
+
+        return messageRepository.saveMessage(foundMessage);
+    }
+
+    @Override
+    public void deleteMessage(UUID sendUserId, UUID messageId) {
+        Message foundMessage = findMessageByIdOrThrow(messageId);
+
+        if (foundMessage.isNotOwner(sendUserId)) {
+            throw new RuntimeException(ErrorMessage.NOT_MESSAGE_CREATOR.getMessage());
+        }
+
+        messageRepository.removeMessage(messageId);
+    }
+}
