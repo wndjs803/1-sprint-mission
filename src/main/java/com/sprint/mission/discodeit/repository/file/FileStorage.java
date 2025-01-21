ae8dd5a768c397c54cc0ee09be28aa8375bfd27e
김주언 <wndjs803@naver.com>
2025-01-21 14:26:34 +0900

    fix: 파일 관련 로직 일부 수정

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileStorage.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileStorage.java
@@ -1,86 +1,88 @@
 package com.sprint.mission.discodeit.repository.file;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+
+import com.sprint.mission.discodeit.common.ErrorMessage;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileStorage {
+
     public void init(Path directory) {
-        // 저장할 경로의 파일 초기화
-        if (!Files.exists(directory)) {
-            try {
+        try {
+            if (!Files.exists(directory)) {
                 Files.createDirectories(directory);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessage.DIRECTORY_INIT_FAIL.format(directory));
         }
     }
 
     public <T> T save(Path filePath, T data) {
-        try (
-                FileOutputStream fos = new FileOutputStream(filePath.toFile());
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
-        ) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(filePath))) {
             oos.writeObject(data);
             return data;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(ErrorMessage.FILE_WRITE_FAIL.format(filePath, data));
         }
     }
 
     public <T> List<T> load(Path directory) {
-        if (Files.exists(directory)) {
-            try {
-                List<T> list = Files.list(directory)
-                        .map(path -> {
-                            try (
-                                    FileInputStream fis = new FileInputStream(path.toFile());
-                                    ObjectInputStream ois = new ObjectInputStream(fis)
-                            ) {
-                                Object data = ois.readObject();
-                                return (T) data;
-                            } catch (IOException | ClassNotFoundException e) {
-                                throw new RuntimeException(e);
-                            }
-                        })
-                        .toList();
+        List<T> list = new ArrayList<>();
+        try {
+            if (!Files.exists(directory)) {
                 return list;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
-        } else {
-            return new ArrayList<>();
+
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+                for (Path file : stream) {
+                    try {
+                        T data = deserialize(file);
+                        list.add(data);
+                    } catch (IOException | ClassNotFoundException e) {
+                        throw new RuntimeException(ErrorMessage.FILE_READ_FAIL.format(file.toString()));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessage.FILES_LOAD_FAIL.format(directory));
         }
+        return list;
     }
 
     public void remove(Path directory, Object object) {
-        if (Files.exists(directory)) {
-            try {
-                Files.list(directory)
-                        .forEach(path -> {
-                            try (
-                                    FileInputStream fis = new FileInputStream(path.toFile());
-                                    ObjectInputStream ois = new ObjectInputStream(fis)
-                            ) {
-                                Object data = ois.readObject();
-                                if (data.equals(object)) {
-                                    Files.deleteIfExists(path);
-                                }
-                            } catch (IOException | ClassNotFoundException e) {
-                                throw new RuntimeException(e);
-                            }
-                        });
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        try {
+            if (!Files.exists(directory)) {
+                return;
             }
-        } else {
 
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+                for (Path file : stream) {
+                    try {
+                        Object data = deserialize(file);
+                        if (data.equals(object)) {
+                            Files.deleteIfExists(file);
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        throw new RuntimeException(ErrorMessage.FILE_REMOVE_FAIL.format(file.toString()));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessage.FILES_LOAD_FAIL.format(directory));
         }
     }
+
+    private <T> T deserialize(Path filePath) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(filePath))) {
+            return (T) ois.readObject();
+        }
+    }
+
 }
