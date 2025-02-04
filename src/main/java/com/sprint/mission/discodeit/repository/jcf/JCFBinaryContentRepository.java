075d0e7d5b0eb459beac8252af2fc4ee563bf1b9
김주언 <wndjs803@naver.com>
2025-02-04 18:17:44 +0900

    feat: JCFBinaryContentRepository 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
@@ -0,0 +1,44 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service("jcfBinaryContentRepository")
+@NoArgsConstructor
+public class JCFBinaryContentRepository implements BinaryContentRepository {
+
+    private final Map<UUID, BinaryContent> binaryContentData = new HashMap<>();
+
+    @Override
+    public BinaryContent saveBinaryContent(BinaryContent binaryContent) {
+        return binaryContentData.put(binaryContent.getId(), binaryContent);
+    }
+
+    @Override
+    public BinaryContent findBinaryContentById(UUID binaryContentId) {
+        return binaryContentData.get(binaryContentId);
+    }
+
+    @Override
+    public List<BinaryContent> findAllBinaryContentById(List<UUID> binaryContentIdList) {
+        List<BinaryContent> binaryContentList = new ArrayList<>();
+
+        for (UUID binaryContentId : binaryContentIdList) {
+            binaryContentList.add(binaryContentData.get(binaryContentId));
+        }
+        return binaryContentList;
+    }
+
+    @Override
+    public void removeBinaryContent(UUID binaryContentId) {
+        binaryContentData.remove(binaryContentId);
+    }
+}
