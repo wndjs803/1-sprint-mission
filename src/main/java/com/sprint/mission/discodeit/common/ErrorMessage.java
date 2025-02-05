6ed23af79f73e9a77f6ca1e669e909f00534bda7
김주언 <wndjs803@naver.com>
2025-02-05 15:15:18 +0900

    feat: updateChannel 메서드 고도화

--- src/main/java/com/sprint/mission/discodeit/common/ErrorMessage.java
+++ src/main/java/com/sprint/mission/discodeit/common/ErrorMessage.java
@@ -7,10 +7,10 @@ public enum ErrorMessage {
 
     // Channel
     CHANNEL_NOT_FOUND("채널을 찾을 수 없습니다. %s"),
-    NOT_CHANNEL_CREATOR("채널 생성자가 아닙니다. id: %s"),
+    NOT_CHANNEL_CREATOR("채널 생성자가 아닙니다. %s"),
     CHANNEL_OWNER_NOT_NULL("채널 소유자는 null일 수 없습니다."),
     USER_NOT_NULL("유저는 null일 수 없습니다."),
-
+    CANNOT_UPDATE_PRIVATE_CHANNEL("PRIVATE 채널은 수정할 수 없습니다. %s"),
 
     // Message
     MESSAGE_NOT_FOUND("메세지 찾을 수 없습니다. id: %s"),
---
+++ src/main/java/com/sprint/mission/discodeit/dto/channel/response/UpdateChannelResponse.java
@@ -0,0 +1,7 @@
+package com.sprint.mission.discodeit.dto.channel.response;
+
+public record UpdateChannelResponse(
+        String name,
+        String description
+) {
+}
