5406def5fd4d4f73eee36d91fc8a1f185e9eac8d
김주언 <wndjs803@naver.com>
2025-02-05 09:10:37 +0900

    feat: login 메서드 로그인 여부 변경 추가

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -3,11 +3,15 @@ package com.sprint.mission.discodeit.service.basic;
 import com.sprint.mission.discodeit.dto.user.request.LoginRequest;
 import com.sprint.mission.discodeit.dto.user.response.LoginResponse;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.mapper.user.UserMapper;
+import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.AuthService;
 import com.sprint.mission.discodeit.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +19,18 @@ public class BasicAuthService implements AuthService {
 
     private final UserValidator userValidator;
     private final UserMapper userMapper;
+    private final UserStatusRepository userStatusRepository;
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
         User user = userValidator.validateUserExistsByNameAndPassword(loginRequest.name(), loginRequest.password());
+
+        // UserStatus 로그인 여부 변경
+        UserStatus userStatus = userStatusRepository.findUserStatusByUser(user);
+        userStatus.updateLoginAt(Instant.now());
+        userStatus.updateOnline();
+        userStatus.updateUpdatedAt(Instant.now());
+
         return userMapper.toLoginResponse(user);
     }
 }
