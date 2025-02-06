d60ede5d2fe755ec7301acf36fb616b33b647717
김주언 <wndjs803@naver.com>
2025-02-06 10:18:35 +0900

    refactor(mentor): entity 생성 시 null 체크

--- src/main/java/com/sprint/mission/discodeit/common/ErrorMessage.java
+++ src/main/java/com/sprint/mission/discodeit/common/ErrorMessage.java
@@ -4,13 +4,14 @@ public enum ErrorMessage {
     // User
     USER_NOT_FOUND("유저를 찾을 수 없습니다. %s"),
     USER_ALREADY_EXIST("이미 유저가 존재합니다. %s"),
+    USER_NOT_NULL("유저는 null일 수 없습니다."),
 
     // Channel
     CHANNEL_NOT_FOUND("채널을 찾을 수 없습니다. %s"),
     NOT_CHANNEL_CREATOR("채널 생성자가 아닙니다. %s"),
     CHANNEL_OWNER_NOT_NULL("채널 소유자는 null일 수 없습니다."),
-    USER_NOT_NULL("유저는 null일 수 없습니다."),
     CANNOT_UPDATE_PRIVATE_CHANNEL("PRIVATE 채널은 수정할 수 없습니다. %s"),
+    CHANNEL_NOT_NULL("채널은 null일 수 없습니다."),
 
     // Message
     MESSAGE_NOT_FOUND("메세지 찾을 수 없습니다. id: %s"),
@@ -25,7 +26,10 @@ public enum ErrorMessage {
     FILE_CONVERSION_FAIL("파일을 바이트 배열로 변환하는 데 실패했습니다. 이유: %s"),
 
     // ReadStatus
-    READSTATUS_NOT_FOUND("readStatus를 찾을 수 없습니다. %s");
+    READSTATUS_NOT_FOUND("readStatus를 찾을 수 없습니다. %s"),
+
+    // BinaryContent
+    BINARYCONTENT_NOT_NULL("BinaryContent는 null일 수 없습니다.");
 
     private final String msg;
 
