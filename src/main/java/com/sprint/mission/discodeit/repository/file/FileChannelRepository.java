7fabb4612768682f843b2bee68be66b7fa356bf2
김주언 <wndjs803@naver.com>
2025-02-07 11:46:40 +0900

    feat: 임시 주석 처리

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
@@ -1,47 +1,47 @@
-package com.sprint.mission.discodeit.repository.file;
-
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.repository.ChannelRepository;
-import org.springframework.stereotype.Repository;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.UUID;
-
-@Repository
-public class FileChannelRepository implements ChannelRepository {
-    private final FileStorage fileStorage;
-    private final Path directory = Paths.get(System.getProperty("user.dir"), "data", "channel");
-
-    public FileChannelRepository(FileStorage fileStorage) {
-        this.fileStorage = fileStorage;
-        fileStorage.init(directory);
-    }
-
-    @Override
-    public Channel saveChannel(Channel channel) {
-        Path filePath = directory.resolve(channel.getId().toString().concat(".ser"));
-        return fileStorage.save(filePath, channel);
-    }
-
-    @Override
-    public Channel findChannelById(UUID channelId) {
-        List<Channel> channelList = fileStorage.load(directory);
-
-        return channelList.stream()
-                .filter(channel -> channel.getId().equals(channelId))
-                .findFirst().get();
-    }
-
-    @Override
-    public List<Channel> findAllChannels() {
-        return fileStorage.load(directory);
-    }
-
-    @Override
-    public void removeChannel(UUID channelId) {
-        Channel channel = findChannelById(channelId);
-        fileStorage.remove(directory, channel);
-    }
-}
+//package com.sprint.mission.discodeit.repository.file;
+//
+//import com.sprint.mission.discodeit.entity.Channel;
+//import com.sprint.mission.discodeit.repository.ChannelRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Repository
+//public class FileChannelRepository implements ChannelRepository {
+//    private final FileStorage fileStorage;
+//    private final Path directory = Paths.get(System.getProperty("user.dir"), "data", "channel");
+//
+//    public FileChannelRepository(FileStorage fileStorage) {
+//        this.fileStorage = fileStorage;
+//        fileStorage.init(directory);
+//    }
+//
+//    @Override
+//    public Channel saveChannel(Channel channel) {
+//        Path filePath = directory.resolve(channel.getId().toString().concat(".ser"));
+//        return fileStorage.save(filePath, channel);
+//    }
+//
+//    @Override
+//    public Channel findChannelById(UUID channelId) {
+//        List<Channel> channelList = fileStorage.load(directory);
+//
+//        return channelList.stream()
+//                .filter(channel -> channel.getId().equals(channelId))
+//                .findFirst().get();
+//    }
+//
+//    @Override
+//    public List<Channel> findAllChannels() {
+//        return fileStorage.load(directory);
+//    }
+//
+//    @Override
+//    public void removeChannel(UUID channelId) {
+//        Channel channel = findChannelById(channelId);
+//        fileStorage.remove(directory, channel);
+//    }
+//}
--- src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -1,53 +1,53 @@
-package com.sprint.mission.discodeit.repository.file;
-
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.repository.MessageRepository;
-import org.springframework.stereotype.Repository;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.UUID;
-
-@Repository
-public class FileMessageRepository implements MessageRepository {
-    private final FileStorage fileStorage;
-    private final Path directory = Paths.get(System.getProperty("user.dir"), "data", "message");
-
-    public FileMessageRepository(FileStorage fileStorage) {
-        this.fileStorage = fileStorage;
-        fileStorage.init(directory);
-    }
-
-    @Override
-    public Message saveMessage(Message message) {
-        Path filePath = directory.resolve(message.getId().toString().concat(".ser"));
-        return fileStorage.save(filePath, message);
-    }
-
-    @Override
-    public Message findMessageById(UUID messageId) {
-        List<Message> messageList = fileStorage.load(directory);
-
-        return messageList.stream()
-                .filter(channel -> channel.getId().equals(messageId))
-                .findFirst().get();
-    }
-
-    @Override
-    public List<Message> findAllMessages() {
-        return fileStorage.load(directory);
-    }
-
-    @Override
-    public List<Message> findAllMessagesByChannel(Channel channel) {
-        return null;
-    }
-
-    @Override
-    public void removeMessage(UUID messageId) {
-        Message message = findMessageById(messageId);
-        fileStorage.remove(directory, message);
-    }
-}
+//package com.sprint.mission.discodeit.repository.file;
+//
+//import com.sprint.mission.discodeit.entity.Channel;
+//import com.sprint.mission.discodeit.entity.Message;
+//import com.sprint.mission.discodeit.repository.MessageRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Repository
+//public class FileMessageRepository implements MessageRepository {
+//    private final FileStorage fileStorage;
+//    private final Path directory = Paths.get(System.getProperty("user.dir"), "data", "message");
+//
+//    public FileMessageRepository(FileStorage fileStorage) {
+//        this.fileStorage = fileStorage;
+//        fileStorage.init(directory);
+//    }
+//
+//    @Override
+//    public Message saveMessage(Message message) {
+//        Path filePath = directory.resolve(message.getId().toString().concat(".ser"));
+//        return fileStorage.save(filePath, message);
+//    }
+//
+//    @Override
+//    public Message findMessageById(UUID messageId) {
+//        List<Message> messageList = fileStorage.load(directory);
+//
+//        return messageList.stream()
+//                .filter(channel -> channel.getId().equals(messageId))
+//                .findFirst().get();
+//    }
+//
+//    @Override
+//    public List<Message> findAllMessages() {
+//        return fileStorage.load(directory);
+//    }
+//
+//    @Override
+//    public List<Message> findAllMessagesByChannel(Channel channel) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void removeMessage(UUID messageId) {
+//        Message message = findMessageById(messageId);
+//        fileStorage.remove(directory, message);
+//    }
+//}
