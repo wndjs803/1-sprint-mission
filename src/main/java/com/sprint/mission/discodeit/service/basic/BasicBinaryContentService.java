c0626d47ce145cd7873bb48b48fe9c0bae3a16fa
김주언 <wndjs803@naver.com>
2025-02-07 10:10:32 +0900

    feat: BasicBinaryContentService 클래스 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -0,0 +1,56 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.common.ErrorMessage;
+import com.sprint.mission.discodeit.common.MultipartFileConverter;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BasicBinaryContentService implements BinaryContentService {
+
+    private final BinaryContentRepository binaryContentRepository;
+    private final MultipartFileConverter multipartFileConverter;
+
+    @Override
+    public BinaryContent createBinaryContent(MultipartFile multipartFile) {
+        byte[] converterByteArray = multipartFileConverter.toByteArray(multipartFile);
+        BinaryContent binaryContent = BinaryContent.of(converterByteArray);
+
+        return binaryContentRepository.saveBinaryContent(binaryContent);
+    }
+
+    @Override
+    public BinaryContent createBinaryContent(BinaryContent binaryContent) {
+        return binaryContentRepository.saveBinaryContent(binaryContent);
+    }
+
+    @Override
+    public BinaryContent findBinaryContentById(UUID binaryContentId) {
+        return Optional.ofNullable(binaryContentRepository.findBinaryContentById(binaryContentId))
+                .orElseThrow(() -> new IllegalArgumentException(
+                        ErrorMessage.BINARYCONTENT_NOT_FOUND.format("id: " + binaryContentId)));
+    }
+
+    @Override
+    public List<BinaryContent> findAllBinaryContentsById(List<UUID> binaryContentIdList) {
+        return binaryContentIdList.stream()
+                .map(binaryContentId -> findBinaryContentById(binaryContentId))
+                .toList();
+    }
+
+    @Override
+    public void deleteBinaryContent(UUID binaryContentId) {
+        findBinaryContentById(binaryContentId);
+
+        binaryContentRepository.removeBinaryContent(binaryContentId);
+    }
+}
