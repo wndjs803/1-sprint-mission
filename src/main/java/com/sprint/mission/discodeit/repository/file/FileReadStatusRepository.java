e0c565e9860c27a777f625d41e45f6997c6e92c2
김주언 <wndjs803@naver.com>
2025-02-07 16:01:59 +0900

    feat: FileReadStatusRepository 구현 및 테스트

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileReadStatusRepository.java
@@ -0,0 +1,55 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import org.springframework.stereotype.Repository;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class FileReadStatusRepository implements ReadStatusRepository {
+
+    private final FileStorage fileStorage;
+    private final Path directory;
+    private static final String SUBDIRECTORY = "readStatus";
+
+    public FileReadStatusRepository(FileStorage fileStorage) {
+        this.fileStorage = fileStorage;
+        directory = fileStorage.getDirectory(SUBDIRECTORY);
+        fileStorage.init(directory);
+    }
+
+
+    @Override
+    public ReadStatus saveReadStatus(ReadStatus readStatus) {
+        Path filePath = fileStorage.getFilePath(directory, readStatus.getId());
+        return fileStorage.save(filePath, readStatus);
+    }
+
+    @Override
+    public ReadStatus findReadStatusById(UUID readStatusId) {
+        List<ReadStatus> readStatusList = fileStorage.load(directory);
+
+        return readStatusList.stream()
+                .filter(readStatus -> readStatus.getId().equals(readStatusId))
+                .findFirst().orElse(null);
+    }
+
+    @Override
+    public Optional<ReadStatus> findReadStatusByUserId(UUID userId) {
+        List<ReadStatus> readStatusList = fileStorage.load(directory);
+
+        return readStatusList.stream()
+                .filter(readStatus -> readStatus.getUser().getId().equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public void removeReadStatus(UUID readStatusId) {
+        ReadStatus readStatus = findReadStatusById(readStatusId);
+        fileStorage.remove(directory, readStatus);
+    }
+}
