0e20430418863375ca4c4318bc65bb36f430d0d1
김주언 <wndjs803@naver.com>
2025-02-17 10:50:37 +0900

    feat: 유저 목록 조회 컨트롤러 구현

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.user.request.CreateUserRequest;
 import com.sprint.mission.discodeit.dto.user.request.UpdateUserRequest;
+import com.sprint.mission.discodeit.dto.user.response.FindUserResponse;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -45,12 +46,14 @@ public class UserController {
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable UUID id) {
         userService.deleteUser(id);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("success");
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body("success");
     }
 
     @GetMapping("")
-    public ResponseEntity<User> findAllUsers() {
-        return null;
+    public ResponseEntity<List<FindUserResponse>> findAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.findAllUsers());
     }
 
     @PatchMapping("/{id}/status")
