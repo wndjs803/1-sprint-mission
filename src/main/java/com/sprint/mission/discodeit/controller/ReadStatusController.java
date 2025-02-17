4171079677e843f618e961475ab6b46fcb70dd54
김주언 <wndjs803@naver.com>
2025-02-17 16:30:12 +0900

    feat: 메세지 수신 정보 수정 컨트롤러 구현

--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -8,10 +8,14 @@ import com.sprint.mission.discodeit.service.ReadStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +30,12 @@ public class ReadStatusController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ResultResponse.of(ResultCode.READSTATUS_CREATED,
                         readStatusService.createReadStatus(createReadStatusRequest)));
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<ResultResponse<ReadStatus>> updateReadStatus(@PathVariable UUID id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResultResponse.of(ResultCode.READSTATUS_UPDATED,
+                        readStatusService.updateReadStatus(id)));
     }
 }
--- src/main/java/com/sprint/mission/discodeit/global/response/ResultCode.java
+++ src/main/java/com/sprint/mission/discodeit/global/response/ResultCode.java
@@ -31,7 +31,8 @@ public enum ResultCode {
     MESSAGE_LIST_FETCHED(200, "메시지 목록 조회에 성공했습니다."),
 
     // ReadStatus
-    READSTATUS_CREATED(201, "read-status 생성에 성공했습니다.");
+    READSTATUS_CREATED(201, "read-status 생성에 성공했습니다."),
+    READSTATUS_UPDATED(200, "read-status 정보 수정에 성공했습니다.");
 
 
     private final int code;
