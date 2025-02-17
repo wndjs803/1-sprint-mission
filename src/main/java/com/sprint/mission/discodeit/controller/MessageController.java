47bc63a1ce32e4768d521787894c68f5506ad506
김주언 <wndjs803@naver.com>
2025-02-17 16:14:58 +0900

    feat: 메세지 목록 조회 컨트롤러 구현

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -11,11 +11,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -55,6 +57,13 @@ public class MessageController {
             @RequestBody DeleteMessageRequest deleteMessageRequest) {
        messageService.deleteMessage(id, deleteMessageRequest);
        return ResponseEntity.status(HttpStatus.OK)
-               .body(ResultResponse.of(ResultCode.CHANNEL_DELETED, true));
+               .body(ResultResponse.of(ResultCode.MESSAGE_DELETED, true));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ResultResponse<List<Message>>> findMessagesByChannel(@RequestParam UUID channelId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResultResponse.of(ResultCode.MESSAGE_LIST_FETCHED,
+                        messageService.findAllMessagesByChannelId(channelId)));
     }
 }
--- src/main/java/com/sprint/mission/discodeit/global/response/ResultCode.java
+++ src/main/java/com/sprint/mission/discodeit/global/response/ResultCode.java
@@ -27,7 +27,8 @@ public enum ResultCode {
     // Message
     MESSAGE_CREATED(201, "메세지 생성에 성공했습니다."),
     MESSAGE_UPDATED(200, "메세지 정보 수정에 성공했습니다."),
-    MESSAGE_DELETED(200, "메세지 삭제에 성공했습니다.");
+    MESSAGE_DELETED(200, "메세지 삭제에 성공했습니다."),
+    MESSAGE_LIST_FETCHED(200, "메시지 목록 조회에 성공했습니다.");
 
 
     private final int code;
