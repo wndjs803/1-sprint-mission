fc990a38521153fd43dfa8cd6b9c4b3e8cde5684
김주언 <wndjs803@naver.com>
2025-02-17 16:42:53 +0900

    feat: 바이너리파일 조회 컨트롤러 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.global.response.ResultCode;
+import com.sprint.mission.discodeit.global.response.ResultResponse;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/binary-content")
+public class BinaryContentController {
+
+    private final BinaryContentService binaryContentService;
+
+    @GetMapping
+    public ResponseEntity<ResultResponse<List<BinaryContent>>> findBinaryContents(
+            @RequestBody List<UUID> binaryContentIdList) {
+        return ResponseEntity.status(HttpStatus.OK)
+                        .body(ResultResponse.of(ResultCode.BINARYCONTENT_LIST_FETCHED,
+                                binaryContentService.findAllBinaryContentsById(binaryContentIdList)));
+    }
+}
