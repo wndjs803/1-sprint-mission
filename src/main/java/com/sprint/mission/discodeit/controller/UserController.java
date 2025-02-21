ae50ad709e326ddac6333a65bfb51f45dedf920f
김주언 <wndjs803@naver.com>
2025-02-21 10:29:07 +0900

    feat: user swagger 설정

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.controller.api.UserApi;
 import com.sprint.mission.discodeit.dto.user.request.CreateUserRequest;
 import com.sprint.mission.discodeit.dto.user.request.UpdateUserRequest;
 import com.sprint.mission.discodeit.dto.user.response.FindUserResponse;
@@ -9,6 +10,8 @@ import com.sprint.mission.discodeit.global.response.ResultCode;
 import com.sprint.mission.discodeit.global.response.ResultResponse;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.UserStatusService;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,57 +23,54 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
-public class UserController {
+public class UserController implements UserApi {
 
-    private final UserService userService;
-    private final UserStatusService userStatusService;
+  private final UserService userService;
+  private final UserStatusService userStatusService;
 
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    public ResponseEntity<ResultResponse<User>> createUser(
-            @RequestPart(value = "createUserRequest") CreateUserRequest createUserRequest,
-            @RequestPart(value = "profileImage") MultipartFile profileImage) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ResultResponse.of(ResultCode.USER_CREATED,
-                        userService.createUser(createUserRequest, profileImage)));
-    }
+  @RequestMapping(value = "", method = RequestMethod.POST)
+  public ResponseEntity<ResultResponse<User>> createUser(
+      @RequestPart(value = "createUserRequest") CreateUserRequest createUserRequest,
+      @RequestPart(value = "profileImage") MultipartFile profileImage) {
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(ResultResponse.of(ResultCode.USER_CREATED,
+            userService.createUser(createUserRequest, profileImage)));
+  }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<ResultResponse<User>> updateUser(
-            @PathVariable UUID id,
-            @RequestPart(value = "updateUserRequest") UpdateUserRequest updateUserRequest,
-            @RequestPart(value = "profileImage") MultipartFile profileImage) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ResultResponse.of(ResultCode.USER_UPDATED,
-                        userService.updateUser(id, updateUserRequest, profileImage)));
-    }
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<ResultResponse<User>> updateUser(
+      @PathVariable UUID id,
+      @RequestPart(value = "updateUserRequest") UpdateUserRequest updateUserRequest,
+      @RequestPart(value = "profileImage") MultipartFile profileImage) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(ResultResponse.of(ResultCode.USER_UPDATED,
+            userService.updateUser(id, updateUserRequest, profileImage)));
+  }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<ResultResponse<UUID>> deleteUser(@PathVariable UUID id) {
-        userService.deleteUser(id);
-        return ResponseEntity.status(HttpStatus.ACCEPTED)
-                .body(ResultResponse.of(ResultCode.USER_DELETED, id));
-    }
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<ResultResponse<UUID>> deleteUser(@PathVariable UUID id) {
+    userService.deleteUser(id);
+    return ResponseEntity.status(HttpStatus.ACCEPTED)
+        .body(ResultResponse.of(ResultCode.USER_DELETED, id));
+  }
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResponseEntity<ResultResponse<List<FindUserResponse>>> findAllUsers() {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ResultResponse.of(ResultCode.USER_LIST_FETCHED,
-                        userService.findAllUsers()));
-    }
+  @RequestMapping(value = "", method = RequestMethod.GET)
+  public ResponseEntity<ResultResponse<List<FindUserResponse>>> findAllUsers() {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(ResultResponse.of(ResultCode.USER_LIST_FETCHED,
+            userService.findAllUsers()));
+  }
 
-    @RequestMapping(value = "/{id}/status", method = RequestMethod.PATCH)
-    public ResponseEntity<ResultResponse<Boolean>> updateUserStatus(
-            @PathVariable UUID id,
-            @RequestBody UpdateUserStatusByUserIdRequest updateUserStatusByUserIdRequest) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ResultResponse.of(ResultCode.USER_ONLINE_STATUS_UPDATED,
-                        userStatusService.updateUserStatusByUserId(
-                                id, updateUserStatusByUserIdRequest).isOnline()));
-    }
+  @RequestMapping(value = "/{id}/status", method = RequestMethod.PATCH)
+  public ResponseEntity<ResultResponse<Boolean>> updateUserStatusByUserId(
+      @PathVariable UUID id,
+      @RequestBody UpdateUserStatusByUserIdRequest updateUserStatusByUserIdRequest) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(ResultResponse.of(ResultCode.USER_ONLINE_STATUS_UPDATED,
+            userStatusService.updateUserStatusByUserId(
+                id, updateUserStatusByUserIdRequest).isOnline()));
+  }
 }
