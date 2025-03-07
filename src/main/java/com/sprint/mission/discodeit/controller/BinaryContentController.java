d92cc2665faf2307ce2196d0325dabe4f14d9687
김주언 <wndjs803@naver.com>
2025-03-07 10:04:36 +0900

    feat: 파일 다운로드 api uri 정의

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -3,11 +3,13 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.controller.api.BinaryContentApi;
 import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentDto;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BinaryContentController implements BinaryContentApi {
 
   private final BinaryContentService binaryContentService;
+  private final BinaryContentStorage binaryContentStorage;
 
   @Override
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -33,5 +36,10 @@ public class BinaryContentController implements BinaryContentApi {
       @RequestParam("binaryContentIds") List<UUID> binaryContentIdList) {
     return ResponseEntity.status(HttpStatus.OK)
         .body(binaryContentService.findAllBinaryContentsById(binaryContentIdList));
+  }
+
+  @GetMapping("{id}/download")
+  public ResponseEntity<?> getBinaryContent(@PathVariable UUID id) {
+    return binaryContentStorage.download(binaryContentService.findBinaryContentById(id));
   }
 }
