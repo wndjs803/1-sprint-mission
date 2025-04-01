a3daf31bad811bddab7ab95221285f54b0ca5197
김주언 <wndjs803@naver.com>
2025-04-01 14:06:31 +0900

    test: saveUser success

--- src/test/java/com/sprint/mission/discodeit/repository/UserRepositoryTest.java
+++ src/test/java/com/sprint/mission/discodeit/repository/UserRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.jpa.user.UserJpaRepository;
 import com.sprint.mission.discodeit.repository.jpa.user.UserRepositoryImpl;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,20 +24,23 @@ public class UserRepositoryTest {
   @Autowired
   UserRepositoryImpl userRepository;
 
-//  @BeforeEach
-//  void setUp() {
-//    userRepository = new UserRepositoryImpl(userJpaRepository);
-//  }
+  @Nested
+  class SaveUserTest {
 
-  @Test
-  void createUserTest() {
-    // given
-    User user = User.of("name1", "email", "test1");
+    @Test
+    void 유저_저장_성공() {
+      // given
+      User user = User.of("name1", "email", "password1");
 
-    // when
-    User savedUser = userRepository.saveUser(user);
+      // when
+      User savedUser = userRepository.saveUser(user);
 
-    // then
-    Assertions.assertNotNull(savedUser);
+      // then
+      assertNotNull(savedUser);
+      assertEquals("name1", savedUser.getName());
+      assertEquals("email", savedUser.getEmail());
+      assertEquals("password1", savedUser.getPassword());
+    }
   }
+
 }
