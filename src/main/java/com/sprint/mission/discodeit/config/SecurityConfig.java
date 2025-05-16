e4cdaaf53f644ae704441d3ec922632b309778d8
김주언 <wndjs803@naver.com>
2025-05-16 17:08:52 +0900

    feat: SecurityFilterChain 정의

---
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                    "/actuator/**",
+                    "/favicon.ico",
+                    "/",
+                    "/error",
+                    "/index.html",
+                    "/assets/**"
+                ).permitAll()
+                .requestMatchers("/**").authenticated()
+            )
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
