edd0b99a17e6aa06cc119f497821b565514a7728
김주언 <wndjs803@naver.com>
2025-05-28 15:50:08 +0900

    feat: swagger Authorization header 추가

--- src/main/java/com/sprint/mission/discodeit/config/SwaggerConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.sprint.mission.discodeit.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "JWT";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -19,6 +25,18 @@ public class SwaggerConfig {
             )
             .servers(List.of(
                 new Server().url("http://localhost:8080").description("로컬 서버")
-            ));
+            ))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME, createJWTTokenScheme()));
+    }
+
+    private SecurityScheme createJWTTokenScheme() {
+        return new SecurityScheme()
+            .name("Authorization")
+            .type(Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(In.HEADER);
     }
 }
